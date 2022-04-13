95b19b26d3d78ae1e5d3e54a3bb079da019b74ac
Algorithms Casts <algorithmspractice@gmail.com>
2022-04-13 12:50:52 -0700

    59. Spiral Matrix II.

--- leetcode/medium/59. Spiral Matrix II.py
+++ leetcode/medium/59. Spiral Matrix II.py
@@ -20,8 +20,28 @@ class Solution:
                 d = (d + 1) % 4
                 dx, dy = directions[d]
                 x, y = x + dx, y + dy
-
             counter += 1
 
         return res
 
+    def generateMatrixSol2(self, n):
+        res = [[0 for _ in range(n)] for _ in range(n)]
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        x, y, d, counter = 0, 0, 0, 1
+
+        while counter <= n * n:
+            res[x][y] = counter
+
+            dx, dy = directions[d]
+            nx, ny = x + dx, y + dy
+
+            if 0 <= nx < n and 0 <= ny < n and res[nx][ny] == 0:
+                x, y = nx, ny
+            else:
+                d = (d + 1) % 4
+                dx, dy = directions[d]
+                x, y = x + dx, y + dy
+
+            counter += 1
+
+        return res
