3113ff98ea30ad0cd1b42b1722d78db0455a3f93
Algorithms Casts <algorithmspractice@gmail.com>
2022-05-03 22:09:48 -0700

    1679. Max Number of K-Sum Pairs.

---
+++ leetcode/medium/1679. Max Number of K-Sum Pairs.py
@@ -0,0 +1,51 @@
+import collections
+
+
+class Solution:
+    def maxOperations(self, nums, k):
+        occ = collections.defaultdict(int)
+        count = 0
+        for num in nums:
+            if occ[k - num] > 0:
+                occ[k - num] -= 1
+                count += 1
+            else:
+                occ[num] += 1
+        return count
+
+    def maxOperationsSol2(self, nums, k):
+        occ = collections.Counter(nums)
+        count = 0
+        for x in occ:
+            count += min(occ[x], occ[k - x])
+        return count // 2
+
+    def maxOperationsSol3(self, nums, k):
+        nums.sort()
+        s, e = 0, len(nums) - 1
+        count = 0
+        while s < e:
+            if nums[s] + nums[e] == k:
+                count += 1
+                s += 1
+                e -= 1
+            elif nums[s] + nums[e] > k:
+                e -= 1
+            else:
+                s += 1
+        return count
+
+    def maxOperationsSol4(self, nums, k):
+        nums.sort()
+        s, e = 0, len(nums) - 1
+        count = 0
+        while s < e and nums[s] < k:
+            if nums[s] + nums[e] == k:
+                count += 1
+                s += 1
+                e -= 1
+            elif nums[s] + nums[e] > k:
+                e -= 1
+            else:
+                s += 1
+        return count
